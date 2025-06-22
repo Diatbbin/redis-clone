2e53e0f458d794299e3805d61d8736de6b1e4354
Lim Diat Bbin <limdiatbbin@Lims-MacBook-Air-2.local>
2025-06-22 23:20:20 +0800

    Add header file

---
+++ include/redis_command_handler.h
@@ -0,0 +1,13 @@
+#ifndef REDIS_COMMAND_HANDLER
+#define REDIS_COMMAND_HANDLER
+
+#include <string>
+
+class RedisCommandHandler {
+public:
+    RedisCommandHandler();
+
+    std::string processCommand(const std::string& commandLine);
+}
+
+#endif
