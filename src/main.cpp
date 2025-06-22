c71984c45830f13f60e147d7223a3cd22e8c5079
Lim Diat Bbin <limdiatbbin@Lims-MacBook-Air-2.local>
2025-06-22 21:15:38 +0800

    Project and TCP/IP setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "include/redis_server.h"
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    int port = 6379;
+    if (argc >= 2) port = std::stoi(argv[1]);
+
+    RedisServer server(port);
+
+    return 0;
+}
