1b83e0157adf7c411104f0d4fddadad3100c86be
Lim Diat Bbin <limdiatbbin@Lims-MacBook-Air-2.local>
2025-06-22 23:18:58 +0800

    Implement command handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include "include/redis_server.h"
 
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main(int argc, char* argv[]) {
     int port = 6379;
     if (argc >= 2) port = std::stoi(argv[1]);
 
     RedisServer server(port);
+
+    // Save the database every 5 mins, persistance storage
+    std::thread persistanceThread([](){
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(300));
+
+            // Save db
+        }
+    });
+
+    persistanceThread.detach();
+
+    server.run();
 
     return 0;
 }
---
+++ src/redis_command_handler.cpp
@@ -0,0 +1,69 @@
+#include "include/redis_command_handler.h"
+
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+std::vector<std::string> parseRespCommand(const std::string& input) {
+    std::vector<std::string> tokens;
+    if (input.empty()) return tokens;
+
+    if (input[0] != '*') {
+        std::istringstream iss(input);
+        std::string token;
+
+        while (iss >> token) {
+            tokens.push_back(token);
+        }
+        return tokens;
+    }
+
+    size_t pos = 0;
+
+    if (input[pos] != '*') return tokens;
+    pos++;
+
+    size_t crlf = input.find("\r\n", pos);
+    if (crlf == std::string::npos) return tokens;
+
+    int numElements = std::stoi(input.substr(pos, crlf - pos));
+    pos = crlf + 2;
+
+    for (int i = 0; i < numElements; i++) {
+        // Break whenever the input is in the incorrect format
+        if (pos >= input.size() || input[pos] != '$') break;
+
+        // Skip the input $
+        pos++; 
+
+        size_t crlf = input.find("\r\n", pos);
+        if (crlf == std::string::npos) break;
+        int len = std::stoi(input.substr(pos, crlf - pos));
+        pos = crlf + 2;
+
+        if (pos + len > input.size()) break;
+        std::string token = input.substr(pos, len);
+        tokens.push_back(token);
+
+        // Skip len and token
+        pos += len + 2;
+    }
+    return tokens;
+}
+
+RedisCommandHandler::RedisCommandHandler() {}
+
+std::string RedisCommandHandler::processCommand(const std::string& commandLine) {
+    // Use RESP Parser
+    auto tokens = parseRespCommand(commandLine);
+    if (tokens.empty()) return "Error: Empty Commands\r\n";
+
+    std::string cmd = tokens[0];
+    std::transform(cmd.begin(), cmd.end(), cmd.begin(), ::toupper);
+    std::ostringstream response;
+
+    // Connect to database 
+
+    // Check commands
+    return response.str();
+}
