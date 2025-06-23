18158dd38a6868e812f6947cafd08409da8dd6c7
Lim Diat Bbin <limdiatbbin@Lims-MacBook-Air-2.local>
2025-06-23 21:37:31 +0800

    Add main for redis cli

---
+++ redis-cli/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    std::string host = "127.0.0.1";
+
+    int port = 6379;
+    int i = 1;
+
+    // Parse command line args for -h and -p 
+    while (i < argc) {
+        std::string arg = argv[i];
+        if (arg == "-h" && i + 1 < argc) {
+            host = argv[++i];
+        } else if (arg == "-p" && i + 1 < argc) {
+            port = std::stoi(argv[++i]);
+        } else {
+            break;
+        }
+        ++i;
+    }
+}
